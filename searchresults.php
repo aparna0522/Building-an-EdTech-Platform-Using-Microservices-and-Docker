16829749abe4c3c2a479d44cb38a13d60f3370fc
Janhavi Choudhari <63536585+Janhavikc@users.noreply.github.com>
2022-06-24 17:40:59 +0530

    Update searchresults.php

--- searchresults.php
+++ searchresults.php
@@ -70,7 +70,7 @@
 								if(isset($_SESSION['userid']))
 									echo '<a class="nav-link" href="course.php"><i class="fas fa-1x fa-user-tie"></i>For Teachers </a>';
 								else 
-									echo '<a class="nav-link" href="http://localhost:5001/login"><i class="fas fa-1x fa-user-tie"></i>For Teachers </a>';
+									echo '<a class="nav-link" href=`{window.location.hostname}`+":5001/login"><i class="fas fa-1x fa-user-tie"></i>For Teachers </a>';
 								?>	
 							
 							</li>
@@ -80,13 +80,13 @@
 									if(isset($_SESSION['userid']))
 										echo '<a class="nav-link btn btn-outline-warning" href="logout.php">Logout</a>';
 									else
-										echo '<a class="nav-link btn btn-outline-warning" href="http://localhost:5001/login">Login</a>'
+										echo '<a class="nav-link btn btn-outline-warning" href=`${window.location.hostname}`+":5001/login">Login</a>'
 								?>
 							</li>
 							<li class="nav-item">
 								<?php
 									if(!isset($_SESSION['userid']))
-										echo '<a class="nav-link btn btn-warning" href="http://localhost:5001/register">Signup</a>';
+										echo '<a class="nav-link btn btn-warning" href=`${window.location.hostname}`+ "/register">Signup</a>';
 								?>
 							</li>
 						</ul>
